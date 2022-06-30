80b8314733beeec938db2693eafca595c7a9768d
olivergav <oliwier.gawlik0@gmail.com>
2022-06-30 22:00:12 +0200

    reducer

---
+++ src/reducer.js
@@ -0,0 +1,16 @@
+import {DECREMENT, INCREMENT, SET} from "./actions";
+
+export const initialState = { count: 0 }
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case INCREMENT:
+            return {count: state.count + 1};
+        case DECREMENT:
+            return {count: state.count - 1};
+        case SET:
+            return {count: action.payload};
+        default:
+            return state;
+    }
+}
